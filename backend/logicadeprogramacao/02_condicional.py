f820f367d00cc9f7cf0a5589e35e8a5fa3251d93
agner maurilio dos santos junior <agner.junior@escola.pr.gov.br>
2025-06-23 09:06:03 -0300

    trabalhando com condições

---
+++ backend/logicadeprogramacao/02_condicional.py
@@ -0,0 +1,10 @@
+nota = int(input("Digite a sua Nota: "))
+
+if  nota >= 80:
+    print("Parabéns!")
+
+elif (nota > 80 and nota < 60) :
+    print("Pode Melhorar!")
+
+elif (nota <= 60):
+    print("Recuperação!")
