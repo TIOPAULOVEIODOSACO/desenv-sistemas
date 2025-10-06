9ca8bd15b511449fb5ae28ee615193da7c323f34
agner maurilio dos santos junior <agner.junor@escola.pr.gov.br>
2025-10-06 08:52:04 -0300

    Sua mensagem de commit aqui

--- backend/logicadeprogramacao/explicacaojson.py
+++ backend/logicadeprogramacao/explicacaojson.py
@@ -1,7 +1,11 @@
-import json
 #{} -> Chaves: Definir um objeto -> Ficha de cadastro -> pessoa: nome,cpf,telefone
+
 #[] -> Colchete: Definir uma lista
+
 # Chave/Valor: Chave Descreve o valor  Chave = telefone  Valor = 4499999999
+
+import json
+
 inventario = []
 
 # Tenta carregar o arquivo existente
@@ -12,12 +16,14 @@ except FileNotFoundError:
     print("Arquivo não encontrado. Um novo será criado.")
 
 # Coleta dados do usuário
+id = int(input("Digite o nome do produto:"))
 nome = input("Digite o nome do produto: ")
 quantidade = int(input("Digite a quantidade: "))
 preco = float(input("Digite o preço: "))
 
 # Cria o novo produto
 novo_produto = {
+    "id" : id,
     "nome": nome,
     "quantidade": quantidade,
     "preco": preco,
@@ -32,3 +38,32 @@ with open("loja.json", "w") as arquivo:
     json.dump(inventario, arquivo, indent=4)
 
 print("Produto cadastrado com sucesso!")
+
+
+id_produto_modificar = int (input("Digite o id para modificar"))
+nova_quantidade = int(input("digite a nova quantidade: "))
+
+try:
+    with open ("loja.json", "r") as arquivo:
+        inventario = json.load(arquivo)
+
+except FileNotFoundError:
+    print("Arquivo não Encontrado")
+
+produto_encontrado = False
+
+for produto in inventario:
+    if produto["id"] == id_produto_modificar:
+        #colocamos a nova quantidade
+        produto["quantidade"] = nova_quantidade
+        produto["em_estoque"] = nova_quantidade > 0 
+        produto_encontrado = True
+        break;  
+
+if not produto_encontrado:
+    print("o produto com id informado não foi encontrado")
+
+else:
+    with open("loja.json","w") as arquivo:
+        json.dump(inventario, arquivo , indent = 4)
+print("O arquivo foi alterado com sucesso!")
