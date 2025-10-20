c74accbf5cefd83aaf90c3f4962fde3667e52c16
agner maurilio dos santos junior <agner.junor@escola.pr.gov.br>
2025-10-20 08:49:32 -0300

    Sua mensagem de commit aqui

---
+++ gerenciador_tarefas/config/tarefas/urls.py
@@ -0,0 +1,6 @@
+frim django.urls import path
+from . import views 
+urlpatterns = [
+
+    path('',views.listar_tarefas, name ='lista_tarefas')
+]
--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from models import Tarefa
+
+from models  import HttpResponse 
+
+
+def listar_tarefas(request):
+    tarefas_salvas = Tarefas.onjects.all()
+    contexto = [
+                    "minhas_tarefas": tarefas_salvas        
+    ]
+
+    return render (request, 'tarefa\lista.html', contexto)
