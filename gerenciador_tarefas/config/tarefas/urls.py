0cb3b8ad53463baca4af531470d005473a04f4d7
agner maurilio dos santos junior <agner.junor@escola.pr.gov.br>
2025-10-21 09:40:42 -0300

    Sua mensagem de commit aqui

--- gerenciador_tarefas/config/tarefas/urls.py
+++ gerenciador_tarefas/config/tarefas/urls.py
@@ -1,6 +1,10 @@
-frim django.urls import path
+from django.urls import path
 from . import views 
 urlpatterns = [
 
-    path('',views.listar_tarefas, name ='lista_tarefas')
+    path('',views.listar_tarefas, name ='lista_tarefas'),
+    #DETALHES DA TAREFA
+    #<int:tarefa_id>- captura um numero da URL
+    path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
+
 ]
--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 # Create your views here.
-from models import Tarefa
+from .models import Tarefa
 
-from models  import HttpResponse 
+from django.http  import HttpResponse 
 
 
 def listar_tarefas(request):
-    tarefas_salvas = Tarefas.onjects.all()
-    contexto = [
-                    "minhas_tarefas": tarefas_salvas        
-    ]
+    tarefas_salvas = Tarefa.objects.all()
+    contexto = {
+        "minhas_tarefas" : tarefas_salvas        
+    }
 
-    return render (request, 'tarefa\lista.html', contexto)
+    return render(request, 'tarefas\lista.html', contexto)
+
+def detalhe_tarefa (request, tarefa_id):
+    tarefa =  get_object_or_404(Tarefa, pk=tarefa_id)
+    return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
