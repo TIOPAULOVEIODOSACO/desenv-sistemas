5223e7823ad01da98cee814b9b980aac576d609a
agner maurilio dos santos junior <agner.junor@escola.pr.gov.br>
2025-10-27 08:58:17 -0300

    Sua mensagem de commit aqui

--- gerenciador_tarefas/config/tarefas/urls.py
+++ gerenciador_tarefas/config/tarefas/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     #<int:tarefa_id>- captura um numero da URL
     path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
 
+    path ('adicionar/', views.adicionar_tarefa, name = 'adicionar_tarefa')
 ]
